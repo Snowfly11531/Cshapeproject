0f24d026dcc851bfd771cc7a56cbf2668f10911d
Snowfly11531 <245134041@qq.com>
2018-06-22 19:48:25 +0800

    secend commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,59 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+    static class Extent {
+        public static int myCount<T>(this IEnumerable<T> list) {
+            int sum = 0;
+            foreach (T i in list) {
+                sum++;
+            }
+            return sum;
+        }
+    }
+    class Mylist : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return 1;
+            yield return 2;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    class Mylist2 : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return 1;
+            yield return 2;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
     class Program
     {
+        private static void print<T>(IEnumerable<T> list) {
+            foreach(T i in list) {
+                Console.WriteLine(i);
+            }
+        }
         static void Main(string[] args)
         {
+            var a = new { b = 1 };
+            Mylist mylist = new Mylist();
+            print(mylist);
+            Console.ReadLine();
         }
     }
 }
